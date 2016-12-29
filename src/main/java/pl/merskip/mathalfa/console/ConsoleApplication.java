2aaec5bfbb6e136d163fcfeae3dda7697ec4fddc
Piotr Merski <merskip@gmail.com>
2016-12-29 16:45:14 +0100

    Odpytywanie o wyrażenie w pętli

--- src/main/java/pl/merskip/mathalfa/console/ConsoleApplication.java
+++ src/main/java/pl/merskip/mathalfa/console/ConsoleApplication.java
@@ -1,25 +1,52 @@
 package pl.merskip.mathalfa.console;
 
 import pl.merskip.mathalfa.base.core.Symbol;
+import pl.merskip.mathalfa.base.core.fragment.FragmentsSplitter;
+import pl.merskip.mathalfa.base.infixparser.PostfixConverter;
+import pl.merskip.mathalfa.base.infixparser.PostfixParser;
 import pl.merskip.mathalfa.base.operation.CalculateOperation;
-import pl.merskip.mathalfa.base.shared.SharedPostfixParser;
+import pl.merskip.mathalfa.base.shared.SharedFragmentsRegister;
 
 import java.util.Scanner;
 
 public class ConsoleApplication {
     
+    private FragmentsSplitter splitter;
+    private PostfixConverter converter;
+    private PostfixParser parser;
+    
+    private Scanner input = new Scanner(System.in);
+    
     public static void main(String[] args) {
         new ConsoleApplication().start();
     }
     
+    private ConsoleApplication() {
+        splitter = new FragmentsSplitter(SharedFragmentsRegister.getInstance());
+        converter = new PostfixConverter(splitter);
+        parser = new PostfixParser(converter);
+    }
+    
     private void start() {
-        System.out.print("Input: ");
         
-        String plainText = new Scanner(System.in).nextLine();
-        Symbol rootSymbol = new SharedPostfixParser().parseAndGetRootSymbol(plainText);
-        Symbol result = new CalculateOperation().executeForResult(rootSymbol);
+        System.out.println("Enter expression to calculate:");
+        System.out.println();
         
-        System.out.println("Result: " + result.toPlainText());
+        while (true) {
+            System.out.print(" >> ");
+            String plainText = input.nextLine();
+            
+            if (plainText.equals("q") || plainText.equals("quit")) {
+                System.out.println("Thanks for your attentions");
+                break;
+            }
+            
+            Symbol rootSymbol = parser.parseAndGetRootSymbol(plainText);
+            Symbol result = new CalculateOperation().executeForResult(rootSymbol);
+    
+            System.out.println("Result: " + result.toPlainText());
+            System.out.println();
+        }
     }
     
 }
